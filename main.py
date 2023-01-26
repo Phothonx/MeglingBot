664d5833b8f11e5d9e59847ddcdfa11cb0b47f57
Phothonx <86020708+Phothonx@users.noreply.github.com>
2023-01-26 15:15:50 +0100

    Added structure and cogs loading

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
-
+# START
 print("""
-----------------------------------------------------
+
  __  __            _ _             ____        _
 |  \/  | ___  __ _| (_)_ __   __ _| __ )  ___ | |_
 | |\/| |/ _ \/ _` | | | '_ \ / _` |  _ \ / _ \| __|
@@ -17,17 +17,33 @@ print("""
 |_|  |_|\___|\__, |_|_|_| |_|\__, |____/ \___/ \__|
              |___/           |___/
              By YagooSRV & Phothonx.
-----------------------------------------------------
-
-Launching MeglingBot...""")
 
 
+Launching MeglingBot...
+----------------------------------------------------""")
 
+# INTENTS
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=megling.getconfig.getPrefix(), intents=intents, help_command=None)
 
+# STATUS
+
+
+# ON READY & COGS
+@bot.event
+async def on_ready():
+    print("----------------------------------------------------\nMeglingBot successfully connected !\nLoading cogs...")
+    path = os.getcwd()
+    for files in os.listdir(f"{path}\\megling\\cogs"):
+        if files.endswith(".py"):   
+            try:
+               await bot.load_extension(f"megling.cogs.{files[:-3]}")
+            except:
+                print(f"Failed to load {files[:-3]} !")
+    print("All cogs loadded !")
+
+# RUN
 try :
     bot.run(megling.getconfig.getToken())
-    print("MeglingBot successfully launched !")
 except :
-    print("MeglingBot failed to launched !")
+    print("MeglingBot failed to connect !")
